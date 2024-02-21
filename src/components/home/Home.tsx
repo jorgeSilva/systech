46e576b460bbf33e207d6aa379c1bd69268ae996
jorgeSilva <jorgelfsilva5@gmail.com>
2024-02-21 13:42:04 -0300

    feat - adição da parte esquerda da tela 'home'

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import style from './Home.module.css'
+import blob from '../../assets/home/blub.svg'
+import iphone from '../../assets/home/Iphone.png'
+import mac from '../../assets/home/MacBook.png'
+import insta from '../../assets/Icon/instagram.svg'
+import twitter from '../../assets/Icon/twitter.svg'
+import whatsapp from '../../assets/Icon/whatsapp.svg'
 
 function Home() {
   return (
-    <div style={{height:'300vh'}}>Home</div>
+    <main className={style.home__container__main}>
+      <section className={style.home__content__main}>
+        <h1 className={style.home__h1}>Desenvolvimento de software para potencializar seu negocio.</h1>
+        <h3 className={style.home__h3}>Transformando ideias em soluções.</h3>
+        <h2 className={style.home__h2}>Seu futuro digital começa aqui.</h2>
+
+        <button className={style.home__button__contato}>Entrar em contato</button>
+
+        <div className={style.home__social__medias__container}>
+          <p>Redes sociais</p>
+          -
+          <div className={style.home__social__medias__content}>
+            <a href="https://www.instagram.com/_systech/">
+              <img className={style.home__social__media} src={insta} alt="instagram SysTech" />
+            </a>
+            <a href="">
+              <img className={style.home__social__media} src={twitter} alt="twitter SysTech" />
+            </a>
+            <a href="https://wa.me/5515997886834">
+              <img className={style.home__social__media} src={whatsapp} alt="Whatsapp SysTech"/>
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <figure className={style.home__container__blob}>
+        <div className={style.home__content__blob}>
+          <img className={style.home__blob} src={blob} alt="conteudo laranja que abrange as imagens" />
+          <img className={style.home__mac} src={mac} alt="Laptop de exemplo." />
+          <img className={style.home__iphone} src={iphone} alt="Celular de exemplo." />
+        </div>
+      </figure>
+    </main>
   )
 }
 
--- src/utils/footer/Footer.tsx
+++ src/utils/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div style={{backgroundColor:'var(--p3)', height:'200px'}}>Footer</div>
   )
 }
 
