287d32154c3336856b34ad5f5043d3c685864b1a
jorgeSilva <jorgelfsilva5@gmail.com>
2024-02-23 15:19:10 -0300

    feat - adição da tela 'saiba-mais' com responsividade

--- src/components/saibaMais/SaibaMais.tsx
+++ src/components/saibaMais/SaibaMais.tsx
@@ -6,16 +6,22 @@ import blob_3 from '../../assets/saibaMais/blob 3.svg'
 import blob_4 from '../../assets/saibaMais/blob 4.svg'
 import device_1_1 from '../../assets/saibaMais/Device 1-1.png'
 import device_1_2 from '../../assets/saibaMais/Device 1-2.png'
-import device_2_1 from '../../assets/saibaMais/device 2-1.png'
 import device_3 from '../../assets/saibaMais/Device 3-1.png'
 import device_4 from '../../assets/saibaMais/Device 4-1.png'
 import whatsapp from '../../assets/Icon/whatsapp.svg'
 
 const SaibaMais = () => {
+
+  const terceiro_elemento = React.useRef<HTMLElement | null>(null)
+
+  // document.addEventListener('scroll', () => {
+  //   // console.log(terceiro_elemento.current?.scrollHeight);
+  //   console.log(window.scrollY);
+  // })  
+
   return (
     <section className={style.saiba__container}>
-
-      <section className={style.saiba__content__card}>
+      <section className={`${style.saiba__content__card} ${style.fade__left}`}>
         <div className={style.saiba__card__left}>
           <h2 className={style.saiba__title}>"<span>Assistencia Social System</span>"</h2>
           <h1 className={style.saiba__conteudo}>Sistema designado ao cadastro de familias referentes ao programa da assistencia social<b>.</b></h1>
@@ -24,10 +30,12 @@ const SaibaMais = () => {
 
         <div className={style.saiba__card__right}>
           <img className={style.saiba__card__blob} src={blob} alt="Background laranja para os despositivos." />
+          <img className={style.device11} src={device_1_1} alt="Imagem dispositivo." />
+          <img className={style.device12} src={device_1_2} alt="Imagem dispositivo." />
         </div>
       </section>
 
-      <section className={style.saiba__content__card}>
+      <section className={`${style.saiba__content__card} ${style.fade__right}`}>
         <div className={style.saiba__card__left__conteudo}>
           <h2 className={style.saiba__title}>"<span>Educação na palma da mão</span>"</h2>
           <h1 className={style.saiba__conteudo}>
@@ -41,7 +49,7 @@ const SaibaMais = () => {
         </div>
       </section>
 
-      <section className={style.saiba__content__card}>
+      <section ref={terceiro_elemento} className={`${style.saiba__content__card} ${style.fade__left}`}>
         <div className={style.saiba__card__left}>
           <h2 className={style.saiba__title}>"<span>Campanha Pesquisa</span>"</h2>
           <h1 className={style.saiba__conteudo}>
@@ -52,11 +60,12 @@ const SaibaMais = () => {
 
         <div className={style.saiba__card__right}>
           <img className={style.saiba__card__blob} src={blob_3} alt="Background laranja para os despositivos." />
+          <img className={style.device11} src={device_3} alt="Imagem dispositivo." />
+
         </div>
-        <div className={style.saiba__card__left}></div>
       </section>
 
-      <section className={style.saiba__content__card}>
+      <section className={`${style.saiba__content__card} ${style.fade__right}`}>
         <div className={style.saiba__card__left__conteudo}>
           <h2 className={style.saiba__title}>"<span>Secretaria Almoxarifado</span>"</h2>
           <h1 className={style.saiba__conteudo}>
@@ -67,8 +76,8 @@ const SaibaMais = () => {
 
         <div className={style.saiba__card__right__conteudo}>
           <img className={style.saiba__card__blob__conteudo} src={blob_4} alt="Background laranja para os despositivos." />
+          <img className={style.device4} src={device_4} alt="Imagem dispositivo." />
         </div>
-        <div className={style.saiba__card__left}></div>
       </section>
 
     </section>  
